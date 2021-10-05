c4b832bc5ec2219f2c8a1b6c3632af05ee47f7d9
pedrosekine <pedrosek@gmail.com>
2021-10-04 21:58:44 -0300

    Exercício 06 completo

--- quinzena03/revisao-js/exercicios.js
+++ quinzena03/revisao-js/exercicios.js
@@ -45,7 +45,20 @@ function retornaQuantidadeElementos(array) {
 
 // EXERCÍCIO 06
 function retornaExpressoesBooleanas() {
+  const booleano1 = true
+  const booleano2 = false
+  const booleano3 = !booleano2 
+  const booleano4 = !booleano3 
 
+  const booleanOperations = [
+    booleano1 && booleano2 && !booleano4,
+    (booleano1 && booleano2) || !booleano3,
+    (booleano2 || booleano3) && (booleano4 || booleano1),
+    !(booleano2 && booleano3) || !(booleano1 && booleano3),
+    !(booleano1) && !(booleano3) || (!booleano4 && booleano3 && booleano3)
+  ]
+
+  return(booleanOperations)
 }
 
 // EXERCÍCIO 07
