71a3f8bffc02ab91d5bf86284e382e6bb37b818d
pedrosekine <pedrosek@gmail.com>
2021-11-22 21:55:13 -0300

    Projeto completo

--- modulo3/projeto-whatslab/projeto-whatslab/src/components/MessageDisplay.js
+++ modulo3/projeto-whatslab/projeto-whatslab/src/components/MessageDisplay.js
@@ -15,23 +15,61 @@ const MessageContainer = styled.div`
   display: flex;
   flex-direction: column;
   align-items: flex-start;
-  padding: 1rem;
   justify-content: flex-end;
+  background-color: #E5DDD5;
 `
 
 const FormContainer = styled.form`
   display: grid;
   grid-template-columns: 1fr 4fr 1fr;
   gap: 1rem;
+  margin: 1rem;
 `
 
 const MessageFlex = styled.div`
-  display: grid;
-  grid-template-columns: 1fr 6fr;
-  gap: 1.5rem;
+  display: flex;
+  flex-direction: column;
+  border: 1px ;
+  border-radius: 5px;
+  padding: 0.5rem 2rem;
+  margin-left: 10%;
+  background-color: white;
+
 `
 
 const UsernameBold = styled.p`
+  font-weight: bold;
+  line-height: 0;
+
+`
+
+const EuMessageFlex = styled.div`
+  display: flex;
+  justify-content: flex-end;
+  width: 90%;
+`
+
+const SelfMessage = styled.p`
+  border: 1px ;
+  padding: 1rem;
+  border-radius: 5px;
+  background-color: #DBF8C6;
+
+`
+
+const UserMessage = styled.p`
+  line-height: 0;
+`
+
+const InputForm = styled.input`
+  border-radius: 5px;
+  border: 1px ;
+`
+
+const SubmitButton = styled.button`
+  border-radius: 5px;
+  border: 1px;
+  padding: 0.5rem;
   font-weight: bold;
 `
 
@@ -103,12 +141,21 @@ export class MessageDisplay extends React.Component {
     render () {
         
         let messageArrayPrint = this.state.messageArray.map(object => {
-          return(
-            <MessageFlex>
-              <UsernameBold onDoubleClick={evt => this.onDoubleClickUsername(object.message)} key={object.username}>{object.username}:</UsernameBold>
-              <p onDoubleClick={evt => this.onDoubleClickUsername(object.message)} key={object.message}>{object.message}</p>
-            </MessageFlex>
-          )
+          if (object.username === "eu") {
+            return(
+              <EuMessageFlex>
+                <SelfMessage onDoubleClick={evt => this.onDoubleClickUsername(object.message)} key={object.message}>{object.message}</SelfMessage>
+              </EuMessageFlex>          
+            )
+          } else {
+            return(
+              <MessageFlex>
+                <UsernameBold onDoubleClick={evt => this.onDoubleClickUsername(object.message)} key={object.username}>{object.username}</UsernameBold>
+                <UserMessage onDoubleClick={evt => this.onDoubleClickUsername(object.message)} key={object.message}>{object.message}</UserMessage>
+              </MessageFlex>
+            )
+          }
+
         })        
 
 
@@ -117,9 +164,9 @@ export class MessageDisplay extends React.Component {
             <MessageContainer>
               {messageArrayPrint}
               <FormContainer onSubmit={this.onSubmitForm}>
-                <input placeholder="Username" value={this.state.valueUsername} onChange={this.onChangeUsername} />
-                <input placeholder="Message" value={this.state.valueMessage} onChange={this.onChangeMessage}/>
-                <button type="submit" onClick={this.onClickButton}>Submit</button>
+                <InputForm placeholder="Username" value={this.state.valueUsername} onChange={this.onChangeUsername} />
+                <InputForm placeholder="Message" value={this.state.valueMessage} onChange={this.onChangeMessage}/>
+                <SubmitButton type="submit" onClick={this.onClickButton}>Submit</SubmitButton>
               </FormContainer>
             </MessageContainer>
         </MainContainer>
