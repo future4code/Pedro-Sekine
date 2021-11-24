206513bfbf4188bb1a3b00b7e0282e3915283158
pedrosekine <pedrosek@gmail.com>
2021-11-23 21:57:09 -0300

    exercícios do dia

---
+++ modulo3/rend-condicional-form/rend-condicional-form/src/pages/Final.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+export class Final extends React.Component {
+    
+    render(){
+        return(
+            <div>
+                <h1>O Formulário Acabou</h1>
+                <p>Muito obrigado por participar! Entraremos em contato!</p>
+            </div>
+
+        )
+    }
+}
